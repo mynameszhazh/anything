d48353efaecbafa61f68c94a37f3787b171f0895
mynameszhazh <1594776259@qq.com>
2022-07-31 23:03:28 +0800

    use provide + inject

---
+++ src/views/test/chilren/subchildren.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{sub}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'subchildren',
+    inject: {sub: {from: 'foo'}, subList: {from: 'list'}},
+    created () {
+      console.log(this.subList);
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/test/test.vue
+++ src/views/test/test.vue
@@ -1,28 +1,39 @@
 <template>
   <div class="box">
-    {{name}}
-    <div class="text">
-      <ul>
-        <li v-for="item of xjhList">{{item.name}}-{{item.age}}</li>
-      </ul>
-    </div>
+    <Children></Children>
+    <button @click="onclick">changefoo</button>
   </div>
 </template>
 
 <script>
-import { getXJHInfo } from '../../api/test/mock'
-  export default {
-    data() {
-      return {
-        name: '123',
-        xjhList: []
-      }
-    },
-    async created () {
-      this.xjhList = await getXJHInfo()
-      console.log(this.xjhList)
-    },
-  }
+import { getXJHInfo } from "../../api/test/mock";
+import Children from "./chilren/index.vue";
+export default {
+  provide() {
+    return {
+      list: this.xjhList,
+      foo: "test, foo",
+    };
+  },
+  data() {
+    return {
+      name: "123",
+      xjhList: [],
+    };
+  },
+  async created() {
+    this.xjhList = await getXJHInfo();
+  },
+  components: {
+    Children,
+  },
+  methods: {
+    onclick() {
+      console.log('change')
+      this.foo = 'change test.foo' 
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
