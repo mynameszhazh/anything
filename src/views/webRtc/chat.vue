5a1bd1dea6b88e5a5547aecf0e6ec13d3586f992
mynameszhazh <1594776259@qq.com>
2023-01-18 00:07:38 +0800

    feat: 搭建chat项目基本结构

--- src/views/webRtc/chat.vue
+++ src/views/webRtc/chat.vue
@@ -1,9 +1,77 @@
 <template>
-  <div>chat</div>
+  <div class="chat">
+    <h1>xjh 视频聊天程序</h1>
+    <div class="btns">
+      <el-input v-model="roomId" placeholder="请输入房间id" />
+      <el-button id="joinBtn" ref="joinBtn" type="primary" @click="handleJoin"
+        >加入</el-button
+      >
+      <el-button id="leaveBtn" ref="leaveBtn" type="danger" @click="handleLeave"
+        >离开</el-button
+      >
+    </div>
+    <div class="videos">
+      <video
+        id="localVideo"
+        ref="localVideo"
+        muted
+        controls
+        autoplay
+        playsinline
+      ></video>
+      <video id="remoteVideo" ref="remoteVideo" autoplay playsinline></video>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      roomId: "",
+      localVideo: null,
+    };
+  },
+  methods: {
+    handleJoin() {
+      let contrains = {
+        audio: true,
+        video: true,
+      };
+      navigator.mediaDevices
+        .getUserMedia(contrains)
+        .then(this.handleJoinSuccess)
+        .catch(this.handleJoinError);
+    },
+    handleJoinSuccess(stream) {
+      this.localVideo = this.$refs.localVideo;
+      this.localVideo.srcObject = stream;
+    },
+    handleJoinError(err) {
+      console.error("errrrr", err);
+    },
+    handleLeave() {
+      if (this.localVideo) {
+        this.localVideo.srcObject = null;
+      }
+    },
+  },
+};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.btns {
+  display: flex;
+  justify-content: flex-start;
+  #joinBtn {
+    margin: 0 20px;
+  }
+}
+.videos {
+  height: 500px;
+  video {
+    width: 200px;
+    height: 200px;
+  }
+}
+</style>
