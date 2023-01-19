92451915a42e281de1279eddd9e80621e23dfdb5
徐建辉 <1594776259@qq.com>
2023-01-19 18:27:42 +0800

    feat: webRtc client

--- src/views/webRtc/chat.vue
+++ src/views/webRtc/chat.vue
@@ -3,72 +3,93 @@
     <h1>xjh 视频聊天程序</h1>
     <div class="btns">
       <el-input v-model="roomId" placeholder="请输入房间id" />
-      <el-button id="joinBtn" ref="joinBtn" type="primary" @click="handleJoin"
-        >加入</el-button
-      >
-      <el-button id="leaveBtn" ref="leaveBtn" type="danger" @click="handleLeave"
-        >离开</el-button
-      >
+      <el-button id="joinBtn" ref="joinBtn" type="primary" @click="handleJoin">加入</el-button>
+      <el-button id="leaveBtn" ref="leaveBtn" type="danger" @click="handleLeave">离开</el-button>
     </div>
-    <div class="videos">
-      <video
-        id="localVideo"
-        ref="localVideo"
-        muted
-        controls
-        autoplay
-        playsinline
-      ></video>
+    <div class="videos" ref='videos'>
+      <video id="localVideo" ref="localVideo" muted controls autoplay playsinline></video>
       <video id="remoteVideo" ref="remoteVideo" autoplay playsinline></video>
     </div>
   </div>
 </template>
 
 <script>
+import { ZeroRTCEngine } from './utils/index'
+import { SIGNAL_TYPE_JOIN, localUserId } from './const/index'
 export default {
   data() {
     return {
-      roomId: "",
+      roomId: '',
       localVideo: null,
-    };
+      localStream: null,
+      zeroRTCEngine: null,
+    }
+  },
+  created() {
+    this.initWebSocket();
   },
   methods: {
+    initWebSocket() {
+      this.zeroRTCEngine = new ZeroRTCEngine('ws://175.24.179.131:8010')
+      this.zeroRTCEngine.createWebsocket()
+    },
     handleJoin() {
+      if(!this.roomId) return
       let contrains = {
         audio: true,
-        video: true,
-      };
+        video: true
+      }
       navigator.mediaDevices
         .getUserMedia(contrains)
         .then(this.handleJoinSuccess)
-        .catch(this.handleJoinError);
+        .catch(this.handleJoinError)
+
+    },
+    dojoin(roomId) {
+      const jsonMsg = {
+        cmd: SIGNAL_TYPE_JOIN,
+        roomId: roomId,
+        uid: localUserId
+      }
+      const message = JSON.stringify(jsonMsg)
+      this.zeroRTCEngine.sendMessage(message)
     },
     handleJoinSuccess(stream) {
-      this.localVideo = this.$refs.localVideo;
-      this.localVideo.srcObject = stream;
+      // 获取成功加入到 房间里面去
+      this.dojoin(this.roomId)
+
+      this.localVideo = this.$refs.localVideo
+      // console.log(this.$refs.joinBtn, this.$refs.videos, 'V')
+      this.localVideo.srcObject = stream
+      this.localStream = stream
     },
     handleJoinError(err) {
-      console.error("errrrr", err);
+      console.error('errrrr', err)
     },
     handleLeave() {
       if (this.localVideo) {
-        this.localVideo.srcObject = null;
+        this.localVideo.srcObject = null
+        this.localStream = null
       }
-    },
-  },
-};
+    }
+
+  }
+}
 </script>
 
 <style lang="less" scoped>
 .btns {
   display: flex;
   justify-content: flex-start;
+
   #joinBtn {
     margin: 0 20px;
   }
 }
+
 .videos {
   height: 500px;
+
   video {
     width: 200px;
     height: 200px;
