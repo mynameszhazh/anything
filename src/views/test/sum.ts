29d537bfac5bef061b2003bd829169303f978c7b
徐建辉 <1594776259@qq.com>
2023-02-27 14:17:37 +0800

    refactor: 抽离代码的整体逻辑

--- src/views/test/sum.ts
+++ src/views/test/sum.ts
@@ -1,50 +1,96 @@
-export function statement(invoice, plays) {
-  let totalAmount = 0
-  let volumeCredits = 0
-  let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  }).format
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
-    // 金钱来自
-    let thisAmount = amountFor(play, perf)
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type)
-      volumeCredits += Math.floor(perf.audience / 5)
-    // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience
-      } seats)\n`
-    totalAmount += thisAmount
+// 这是一个固定不变的东西
+export let plays = {
+  hamlet: { name: 'Hamlet', type: 'tragedy' },
+  'as-like': { name: 'As You Like It', type: 'comedy' },
+  othello: { name: 'Othello', type: 'tragedy' }
+}
+export let invoices = [
+  {
+    customer: 'BigCo',
+    performances: [
+      {
+        playID: 'hamlet',
+        audience: 55
+      },
+      {
+        playID: 'as-like',
+        audience: 35
+      },
+      {
+        playID: 'othello',
+        audience: 40
+      }
+    ]
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
-  result += `You earned ${volumeCredits} credits\n`
+]
+
+export function statement(invoice) {
+  let result = `Statement for ${invoice.customer}\n`
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience
+      } seats)\n`
+  }
+
+  result += `Amount owed is ${usd(totalAmount(invoice))}\n`
+  result += `You earned ${totalVolumeCredits(invoice)} credits\n`
   return result
 }
 
-
-function amountFor(play: any, perf: any) {
+function totalAmount(invoice) {
   let result = 0
-  switch (play.type) {
+  for (let perf of invoice.performances) {
+    result += amountFor(perf)
+  }
+  return result
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber / 100);
+}
+
+function totalVolumeCredits(invoice) {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
+function volumeCreditsFor(perf: any) {
+  let result = 0;
+  result += Math.max(perf.audience - 30, 0)
+  // add extra credit for every ten comedy attendees
+  if ('comedy' === playFor(perf).type)
+    result += Math.floor(perf.audience / 5)
+  return result
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function amountFor(aPerformance: any) {
+  let result = 0
+  switch (playFor(aPerformance).type) {
     case 'tragedy':
       result = 40000
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30)
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30)
       }
       break
     case 'comedy':
       result = 30000
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20)
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20)
       }
-      result += 300 * perf.audience
+      result += 300 * aPerformance.audience
       break
     default:
-      throw new Error(`unknown type: ${play.type}`)
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`)
   }
   return result
 }
