647e8c90cfab4557b4b6c64019448af79020eb7f
徐建辉 <1594776259@qq.com>
2023-02-27 11:15:32 +0800

    持续测试 statement 函数sum

--- src/views/test/__tests__/index.ts
+++ src/views/test/__tests__/index.ts
@@ -1,8 +1,32 @@
 // import {describe, expect, test} from '@jest/globals';
-import {sum} from '../sum';
+import { statement } from '../sum';
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('happy path', () => {
+  let plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' }
+  }
+  let invoices = [
+    {
+      customer: 'BigCo',
+      performances: [
+        {
+          playID: 'hamlet',
+          audience: 55
+        },
+        {
+          playID: 'as-like',
+          audience: 35
+        },
+        {
+          playID: 'othello',
+          audience: 40
+        }
+      ]
+    }
+  ]
+  test('statement 函数重构', () => {
+    expect(statement(invoices[0], plays)).toMatchSnapshot();
   });
 });
--- src/views/test/sum.ts
+++ src/views/test/sum.ts
@@ -1,3 +1,50 @@
-export function sum(a: number, b: number): number {
-  return a + b
+export function statement(invoice, plays) {
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `Statement for ${invoice.customer}\n`
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  }).format
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID]
+    // 金钱来自
+    let thisAmount = amountFor(play, perf)
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0)
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === play.type)
+      volumeCredits += Math.floor(perf.audience / 5)
+    // print line for this order
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience
+      } seats)\n`
+    totalAmount += thisAmount
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  return result
+}
+
+
+function amountFor(play: any, perf: any) {
+  let result = 0
+  switch (play.type) {
+    case 'tragedy':
+      result = 40000
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30)
+      }
+      break
+    case 'comedy':
+      result = 30000
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20)
+      }
+      result += 300 * perf.audience
+      break
+    default:
+      throw new Error(`unknown type: ${play.type}`)
+  }
+  return result
 }
