9f297ab8320cef513320121e571a2874e6c3968e
徐建辉 <1594776259@qq.com>
2023-05-06 15:14:47 +0800

    ci: 基本确定handlebars的使用方式

--- extra/template/handlebars/index.js
+++ extra/template/handlebars/index.js
@@ -1,24 +1,56 @@
-const fs = require("fs");
-// import fs from 'fs' 
-const handlebars = require("handlebars");
+const fs = require('fs')
+// import fs from 'fs'
+const Handlebars = require('handlebars')
+var myHandlebars = Handlebars.create({ 
+  // 修改插值符号
+  delimiters: ['${{', '}}'] 
+});
+
+myHandlebars.registerHelper('equal', function (v1, v2, options) {
+  if (v1 == v2) {
+    return options.fn(this)
+  } else {
+    return options.inverse(this)
+  }
+})
 
 function compilerFn(meta, filePath, templatePath) {
   if (fs.existsSync(templatePath)) {
-    const content = fs.readFileSync(templatePath).toString();
-    const result = handlebars.compile(content)(meta);
-    fs.writeFileSync(filePath, result);
+    const content = fs.readFileSync(templatePath).toString()
+    const result = myHandlebars.compile(content)(meta)
+    fs.writeFileSync(filePath, result)
   }
 }
 
-compilerFn({
-  list: [
-    {
-      name: 123,
-      title: 'title'
+compilerFn(
+  {
+    test: 'test',
+    isType(type) {
+      return type === 'input'
     },
-    {
-      name: 456,
-      title: 'title2'
-    },
-  ]
-}, './index.vue', './App.vue.hbs')
+    search: [
+      {
+        type: 'input',
+        title: '商家名称',
+        name: 'sjmc'
+      },
+      {
+        type: 'select',
+        title: '是否优报',
+        name: 'sfyb'
+      },
+      {
+        type: 'select',
+        title: '是否优报3',
+        name: 'sfyb3'
+      },
+      {
+        type: 'select',
+        title: '是否优报2',
+        name: 'sfyb2'
+      }
+    ]
+  },
+  './index.vue',
+  './App.vue.hbs'
+)
