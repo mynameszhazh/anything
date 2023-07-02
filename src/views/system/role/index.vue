f81b79d18204eec7f7ae4b0cb3cf368fb9b6ef28
mynameszhazh <1594776259@qq.com>
2023-07-02 22:38:58 +0800

    ci: 成功对接基本的本地部门管理的相关问题

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -3,14 +3,8 @@
     <!-- search -->
     <div class="search">
       <el-form :inline="true" :model="searchForm" class="demo-form-inline">
-        <el-form-item label="审批人">
-          <el-input v-model="searchForm.user" placeholder="审批人"></el-input>
-        </el-form-item>
-        <el-form-item label="活动区域">
-          <el-select v-model="searchForm.region" placeholder="活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
+        <el-form-item label="部门">
+          <el-input v-model="searchForm.name" placeholder="部门"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="search">查询</el-button>
@@ -20,10 +14,25 @@
 
     <!-- table -->
     <div class="content">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期"> </el-table-column>
+      <div class="handle">
+        <el-button type="primary" @click="add">新增</el-button>
+        <el-button type="danger" @click="allDel">批量删除</el-button>
+      </div>
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column prop="createTime" label="创建时间"> </el-table-column>
         <el-table-column prop="name" label="姓名"> </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column prop="updateTime" label="更新时间"> </el-table-column>
+        <el-table-column prop="detial" label="操作">
+          <template slot-scope="{ row }">
+            <el-button @click="edit(row)">修改</el-button>
+            <el-button type="danger" @click="del(row)">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <el-pagination
         layout="prev, pager, next"
@@ -37,24 +46,24 @@
     <el-dialog
       title="提示"
       :visible.sync="dialogVisible"
-      width="30%"
+      width="700px"
       :before-close="handleClose"
     >
       <el-form :inline="true" :model="formData" class="demo-form-inline">
         <el-row>
           <el-col :span="12">
-            <el-form-item label="审批人">
-              <el-input v-model="formData.user" placeholder="审批人"></el-input>
+            <el-form-item label="部门">
+              <el-input v-model="formData.name" placeholder="部门"></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="12">
+          <!-- <el-col :span="12">
             <el-form-item label="活动区域">
               <el-select v-model="formData.region" placeholder="活动区域">
                 <el-option label="区域一" value="shanghai"></el-option>
                 <el-option label="区域二" value="beijing"></el-option>
               </el-select>
             </el-form-item>
-          </el-col>
+          </el-col> -->
         </el-row>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -80,7 +89,8 @@ export default {
         region: ''
       },
       formData: {},
-      tableData: []
+      tableData: [],
+      multipleSelection: []
     }
   },
   created() {
@@ -92,15 +102,55 @@ export default {
         url: '/api/depts',
         method: 'get'
       }).then((res) => {
-        console.log(res)
+        this.tableData = res.data
       })
+    },
+    add() {
+      console.log('add')
+      this.dialogVisible = true
+    },
+    edit(row) {
+      console.log('edit')
+      this.dialogVisible = true
+      this.formData = row
+    },
+    allDel() {
+      console.log('adddel')
+    },
+    del(row) {
+      console.log('del')
+      this.$confirm('是否删除这个', '删除')
+        .then(() => {
+          this.$http({
+            url: '/api/depts/' + row.id,
+            method: 'delete'
+          }).then((res) => {
+            this.search()
+            this.$message.success('删除成功')
+          })
+        })
+        .catch(() => {
+          this.$message.warning('取消了删除')
+        })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
     },
     handleClose() {
       this.dialogVisible = false
+      this.formData = {}
     },
     onSubmit() {
-      this.dialogVisible = false
-      console.log('submit!')
+      console.log('submit!', this.formData)
+      this.$http({
+        url: '/api/depts/' + (this.formData.id ? 'update' : 'add'),
+        method: 'post',
+        data: this.formData
+      }).then((res) => {
+        this.$message.success('操作成功')
+        this.search()
+        this.dialogVisible = false
+      })
     },
     currentPageChange(page) {
       pagination.page = page
