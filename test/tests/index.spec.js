0eb75ec0d4520215523b708a525093a711745563
mynameszhazh <1594776259@qq.com>
2024-01-03 10:42:22 +0800

    ci: 增加测试标准模板

--- test/tests/index.spec.js
+++ test/tests/index.spec.js
@@ -3,22 +3,10 @@ import { countWords } from '../index.js'
 
 describe('map', () => {
   it('example 1', () => {
-    const words1 = ['leetcode', 'is', 'amazing', 'as', 'is']
-    const words2 = ['amazing', 'leetcode', 'is']
-    const ret = countWords(words1, words2)
-    expect(ret).toBe(2)
   })
 
   it('example 2', () => {
-    const words1 = ['b', 'bb', 'bbb']
-    const words2 = ['a', 'aa', 'aaa']
-    const ret = countWords(words1, words2)
-    expect(ret).toBe(0)
   })
   it('example 3', () => {
-    const words1 = ['a', 'ab']
-    const words2 = ['a', 'a', 'a', 'ab']
-    const ret = countWords(words1, words2)
-    expect(ret).toBe(1)
   })
 })
