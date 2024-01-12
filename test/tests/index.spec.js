32c0a0ef3851d4c9fed429fbb2fd0bb5bf6c19f8
mynameszhazh <1594776259@qq.com>
2024-01-12 11:41:59 +0800

    ci: 增加一些不一样的处理

--- test/tests/index.spec.js
+++ test/tests/index.spec.js
@@ -1,12 +1,30 @@
 import { it, expect, describe } from 'vitest'
-import {  } from '../index.js'
+import fn from '../index.js'
 
 describe('map', () => {
   it('example 1', () => {
+    const a = 'leetscode'
+    const b = ['leet', 'code', 'leetcode']
+    // expect(minExtraChar(a)).toBe(2["hello","world"])
+    expect(fn(a, b)).toBe(1)
+    // expect(canSeePersonsCount(a)).toStrictEqual()
+    // expect(canSeePersonsCount(a, b)).toStrictEqual()
   })
 
   it('example 2', () => {
+    const a = 'sayhelloworld'
+    const b = ['hello', 'world']
+    // expect(minExtraChar(a)).toBe(1)
+    expect(fn(a, b)).toBe(3)
+    // expect(canSeePersonsCount(a)).toStrictEqual()
+    // expect(canSeePersonsCount(a, b)).toStrictEqual()
   })
-  it('example 3', () => {
-  })
+  // it('example 3', () => {
+  // const a = "sayhelloworld"
+  // const b = ["leet","code","leetcode"]
+  // expect(minExtraChar(a)).toBe(1)
+  // expect(minExtraChar(a, b)).toBe(1)
+  // expect(canSeePersonsCount(a)).toStrictEqual()
+  // expect(canSeePersonsCount(a, b)).toStrictEqual()
+  // })
 })
