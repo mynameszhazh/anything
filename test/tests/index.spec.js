e8cdabbfa8ddcd72ec3b820be2118ca4cce8adb9
mynameszhazh <1594776259@qq.com>
2024-01-15 10:28:17 +0800

    doc: add some algorithm note

--- test/tests/index.spec.js
+++ test/tests/index.spec.js
@@ -3,28 +3,29 @@ import fn from '../index.js'
 
 describe('map', () => {
   it('example 1', () => {
-    const a = 'leetscode'
-    const b = ['leet', 'code', 'leetcode']
-    // expect(minExtraChar(a)).toBe(2["hello","world"])
-    expect(fn(a, b)).toBe(1)
-    // expect(canSeePersonsCount(a)).toStrictEqual()
-    // expect(canSeePersonsCount(a, b)).toStrictEqual()
+    const a = 'zzzzdddd'
+    const b = 2
+    // expect(fn(a)).toBe(2["hello","world"])
+    expect(fn(a, b)).toBe('zzdzzdd')
+    // expect(fn(a)).toStrictEqual()
+    // expect(fn(a, b)).toStrictEqual()
   })
 
   it('example 2', () => {
-    const a = 'sayhelloworld'
-    const b = ['hello', 'world']
-    // expect(minExtraChar(a)).toBe(1)
-    expect(fn(a, b)).toBe(3)
-    // expect(canSeePersonsCount(a)).toStrictEqual()
-    // expect(canSeePersonsCount(a, b)).toStrictEqual()
+    const a = "cczazcc"
+    const b = 3
+    // expect(fn(a)).toBe(1)
+    expect(fn(a, b)).toBe('zzcccac')
+    // expect(fn(a)).toStrictEqual()
+    // expect(fn(a, b)).toStrictEqual()
   })
-  // it('example 3', () => {
-  // const a = "sayhelloworld"
-  // const b = ["leet","code","leetcode"]
-  // expect(minExtraChar(a)).toBe(1)
-  // expect(minExtraChar(a, b)).toBe(1)
-  // expect(canSeePersonsCount(a)).toStrictEqual()
-  // expect(canSeePersonsCount(a, b)).toStrictEqual()
-  // })
+
+  it('example 3', () => {
+    const a = "aababab"
+    const b = 2
+    // expect(fn(a)).toBe(1)
+    expect(fn(a, b)).toBe('bbabaa')
+    // expect(fn(a)).toStrictEqual()
+    // expect(fn(a, b)).toStrictEqual()
+  })
 })
